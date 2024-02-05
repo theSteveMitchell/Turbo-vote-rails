a7d4c835449f1fd1f142ed516144accab38f76dd
Steve Mitchell <thestevemitchell@gmail.com>
2024-02-05 18:09:25 -0500

    forms for votes and write-ins, results list, cleanup boilerplate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,4 @@
 class CandidatesController < ApplicationController
-  before_action :set_candidate, only: :show
 
   # GET /candidates or /candidates.json
   def index
@@ -14,20 +13,16 @@ class CandidatesController < ApplicationController
   # POST /candidates or /candidates.json
   def create
     @candidate = Candidate.new(candidate_params)
-
+    @candidate.votes.build
+    
     if @candidate.save
-      redirect_to candidate_url(@candidate), notice: "Candidate was successfully created."
+      redirect_to votes_url, notice: "Candidate was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_candidate
-      @candidate = Candidate.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def candidate_params
       params.require(:candidate).permit(:name)
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,10 @@
 class VotesController < ApplicationController
-  before_action :set_vote, only: :show
 
   # GET /votes or /votes.json
   def index
-    @votes = Vote.all
+    @vote_counts = Vote.joins(:candidate).group("candidates.name").count.map do |k,v|
+      {candidate_name: k, votes: v}
+    end
   end
 
   # GET /votes/new
@@ -16,18 +17,13 @@ class VotesController < ApplicationController
     @vote = Vote.new(vote_params)
 
     if @vote.save
-      redirect_to vote_url(@vote), notice: "Vote was successfully created."
+      redirect_to votes_url, notice: "Vote was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_vote
-      @vote = Vote.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def vote_params
       params.require(:vote).permit(:candidate_id)
