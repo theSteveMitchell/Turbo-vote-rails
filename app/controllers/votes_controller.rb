2aa40c0c9a10175896ff97dd79263281c85cd04e
Steve Mitchell <thestevemitchell@gmail.com>
2024-02-06 18:53:47 -0500

    update results only after every 10th vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,16 @@
 class VotesController < ApplicationController
+  VOTE_MASK = 10
 
   # GET /votes or /votes.json
+  # lists vote tallies, but only update after every 10 votes
+  # e.g when 12 votes are counted, we tally only the first 10. when 89 votes counted, we tally only the first 80.
+  # this is for voter privacy - onlookers can't infer a single vote by watching for update
+  # 
+  # pluck() pulls the AR::Relation into an array in memory, so this won't scale well beyond a few thousand votes.
   def index
-    @vote_counts = Vote.joins(:candidate).group("candidates.name").count.map do |k,v|
+    reveal_count = Vote.count - (Vote.count % VOTE_MASK)
+    @vote_counts = Vote.limit(reveal_count).includes(:candidate).pluck("candidates.name")
+      .sort.tally.map do |k,v|
       {candidate_name: k, votes: v}
     end
   end
