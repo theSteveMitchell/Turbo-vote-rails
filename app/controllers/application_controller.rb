3e9262769f789ffee599f3cdb241b094857917bc
Steve Mitchell <thestevemitchell@gmail.com>
2024-02-07 11:54:52 -0500

    controllers also check session expiry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def authenticate_voter!
-    redirect_to new_session_url unless current_voter_id
+    redirect_to new_session_url unless session_active?
   end
 end
